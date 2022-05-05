c6f05b7a078f2c16074eafb4ce43b8b9cbcea02a
devwulff <p.wulff887@gmail.com>
2022-05-05 14:00:44 +0200

    Subscribe Page optimized and Contact Form added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 let inputmail = document.getElementById('inputmail');
 let inputname = document.getElementById('inputname');
 let popupwindow = document.getElementById('popupwindow');
-
-console.log('it works!');
+let contactname = document.getElementById('contactname');
+let msg = document.getElementById('msg');
 
 function subscritionWindow() {
     console.log(inputmail.value);
@@ -10,7 +10,18 @@ function subscritionWindow() {
     popupwindow.style.visibility = "visible";
 }
 
+function contactWindow() {
+    console.log(contactname.value);
+    console.log(msg.value);
+    contactwindow.style.visibility = "visible";
+}
+
 function closePopup() {
     popupwindow.style.visibility = "hidden";
     inputmail.value = '';
+}
+
+function closecontact() {
+    contactwindow.style.visibility = "hidden";
+    contactname.value = '';
 }
